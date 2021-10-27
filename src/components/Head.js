f328fa9372702ccbbd926c56e68b718415fec218
3bdalah <3bdallah.dev@gmail.com>
2021-10-27 10:57:55 +0200

    done task

---
+++ src/components/Head.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Launches from './ListCards';
+
+const Head = () => {
+ 
+    return (
+      <>
+       {/* <h2>SpaceX Task</h2> */}
+      </>
+    );
+}
+
+export default Head
